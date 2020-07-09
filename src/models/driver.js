90a1edab75570d231fc4dc52adf9b16b90fee28b
Simon <simonbor@gmail.com>
2020-07-09 14:22:52 +0300

    models perform

---
+++ src/models/driver.js
@@ -0,0 +1,9 @@
+module.exports = class Driver{
+	constructor({DriverId, MobileNum, Name, CarName, CarColor}) {
+        this.DriverId = DriverId;
+        this.MobileNum = MobileNum;
+        this.Name = Name;
+        this.CarName = CarName;
+        this.CarColor = CarColor
+    }
+}
