6a23f5a10c4a648c2104bf5726f8b93c777e8eec
Simon <simonbor@gmail.com>
2020-07-12 09:25:50 +0300

    cipher refactor

--- src/cipher.js
+++ src/cipher.js
@@ -2,21 +2,23 @@
 const crypto = require('crypto');
 
 const algorithm = 'aes-128-cbc';
+const inputEncoding = 'utf8';
+const outputEncoding = 'hex';
 const cryptoKey = process.env.CRYPTO_KEY || 'b2df428b9929d3ace7c598bbf4e496b2';
-const key = Buffer.from(cryptoKey, 'hex');
+const key = Buffer.from(cryptoKey, outputEncoding);
 const iv = Buffer.from(crypto.randomBytes(16));
 
 function encrypt(text) {
     const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encrypted = cipher.update(text, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
+    let encrypted = cipher.update(text, inputEncoding, outputEncoding);
+    encrypted += cipher.final(outputEncoding);
     return encrypted;
 }
 
 function decrypt(text) {
     const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decrypted = decipher.update(text, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
+    let decrypted = decipher.update(text, outputEncoding, inputEncoding);
+    decrypted += decipher.final(inputEncoding);
     return decrypted;
 }
 
