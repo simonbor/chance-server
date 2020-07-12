9e5cbb3d18f6499e92f3e2b7b477f1ab21fedd8e
Simon <simonbor@gmail.com>
2020-07-12 16:08:23 +0300

    cipher fix

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,20 @@
 'use strict'
 const crypto = require('crypto');
 
-const algorithm = 'aes-128-cbc';
+const algorithm = 'aes-256-ctr';
 const inputEncoding = 'utf8';
 const outputEncoding = 'hex';
-const cryptoKey = process.env.CRYPTO_KEY || 'b2df428b9929d3ace7c598bbf4e496b2';
-const key = Buffer.from(cryptoKey, outputEncoding);
-const iv = Buffer.from(crypto.randomBytes(16));
+const cryptoKey = process.env.CRYPTO_KEY || 'secretPassword';
 
 function encrypt(text) {
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    const cipher = crypto.createCipher(algorithm, cryptoKey);
     let encrypted = cipher.update(text, inputEncoding, outputEncoding);
     encrypted += cipher.final(outputEncoding);
     return encrypted;
 }
 
 function decrypt(text) {
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    const decipher = crypto.createDecipher(algorithm, cryptoKey);
     let decrypted = decipher.update(text, outputEncoding, inputEncoding);
     decrypted += decipher.final(inputEncoding);
     return decrypted;
