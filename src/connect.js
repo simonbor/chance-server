2f5f8491f90fa64d8fa5128c862dd0f4b04749d1
Simon <simonbor@gmail.com>
2020-07-19 11:17:16 +0300

    fix connect

--- src/connect.js
+++ src/connect.js
@@ -3,9 +3,8 @@ const cipher = require('./cipher');
 const config = require('./config');
 
 const getPool = async function () {
-    if(process.env.NODE_ENV === 'production') {
-        config.config.password = cipher.decrypt(config.config.password);
-    }
+    // decrypt config password and connect
+    config.config.password = cipher.decrypt(config.config.password);
     const pool = await mssql.connect(config.config);
 
     return pool;
