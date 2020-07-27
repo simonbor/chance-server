00f0b76ac030439548683fb6fdd4d95848f68222
Simon <simonbor@gmail.com>
2020-07-27 08:27:59 +0300

    fix connect

--- src/connect.js
+++ src/connect.js
@@ -3,9 +3,12 @@ const cipher = require('./cipher');
 const config = require('./config');
 
 const getPool = async function () {
+    
+    const tmpConfig =JSON.parse(JSON.stringify(config));
+
     // decrypt config password and connect
-    config.config.password = cipher.decrypt(config.config.password);
-    const pool = await mssql.connect(config.config);
+    tmpConfig.config.password = cipher.decrypt(tmpConfig.config.password);
+    const pool = await mssql.connect(tmpConfig.config);
 
     return pool;
 }
