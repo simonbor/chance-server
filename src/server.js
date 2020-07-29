313bcfa55c80ce0aae40a5ca934cd167fe947eb5
Simon <simonbor@gmail.com>
2020-07-29 14:33:42 +0300

    heroku deploy

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 
 const host = '0.0.0.0';
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
     const router = require('./router');
