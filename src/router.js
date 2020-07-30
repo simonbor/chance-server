ee2bfc8b7a7bb3b5dee41c62bba8b4c388ac22eb
Simon <simonbor@gmail.com>
2020-07-30 11:11:15 +0300

    test heroku

--- src/router.js
+++ src/router.js
@@ -24,8 +24,6 @@ function collectRequestDataAsync(request) {
 const route = async function(req, res) {
     const reqUrl = url.parse(req.url, true);
 
-    console.log([req.url, reqUrl.pathname, req.method]);
-
     if (req.method === 'POST') {
         req.body = await collectRequestDataAsync(req);
     }
