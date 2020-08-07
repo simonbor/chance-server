05aa0ce1bad77e6610e4a51c8727edffb73c9aa6
Simon <simonbor@gmail.com>
2020-08-07 18:22:10 +0300

    postgres dal context

--- src/dal/dal-context/psqlContext.js
+++ src/dal/dal-context/psqlContext.js
@@ -3,6 +3,22 @@ const connect = require('../../connect');
 
 module.exports = class MssqlContext {
     async execute(procedureName, request) {
-        throw new Error(`PsqlContext isn't implemented`);
+        const pool = await connect.getPool('psql');
+
+        let csPayload = '';
+        const arrParams = Object.values(request).map(item => {return item.value || null});
+
+        arrParams.map((item, index) => {
+            csPayload += `$${index + 1}, `;
+        })
+        const csDollars = csPayload.trim().slice(0, -1)
+
+        // console.log(arrParams);
+        // console.log(procedureName);
+
+        const res = await pool.query(`select row_to_json(${procedureName}(${csDollars}))`, arrParams);
+        const data = res.rows[0] && res.rows[0]['row_to_json'] || res.rows;
+
+        return data;
     }
 }
