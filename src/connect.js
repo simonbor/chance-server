cce706120c68db56f16d46301cf5e1f0c2b023e2
Simon <simonbor@gmail.com>
2020-08-07 18:30:51 +0300

    postgres mssql switch compatibility

--- src/connect.js
+++ src/connect.js
@@ -1,15 +1,16 @@
 const mssql = require('mssql');
 const cipher = require('./cipher');
 const config = require('./config');
+const { Pool } = require('pg');
 
 // todo: after complete postgres integration don't forget to remove default db value
-const getPool = async function(db = 'mssql') {
-
-    const dbConfig = JSON.parse(JSON.stringify(config[`${db}-config`]));
-
-    // decrypt config password and connect
+const getPool = async function(db) {
+    let pool;
+    const dbConfig = JSON.parse(JSON.stringify(config[`${db}-config`]));    
     dbConfig.password = cipher.decrypt(dbConfig.password);
-    const pool = await mssql.connect(dbConfig);
+
+    (db === 'mssql') && (pool = await mssql.connect(dbConfig));
+    (db === 'psql') && (pool = new Pool(dbConfig));
 
     return pool;
 }
