8567dc4ca49d2df261554e95289383845263a60f
Simon <simonbor@gmail.com>
2020-08-11 11:28:34 +0300

    chance controller update

--- src/controllers/chanceController.js
+++ src/controllers/chanceController.js
@@ -1,7 +1,20 @@
 const driverDal = require('../dal/driverDal');
 const addressDal = require('../dal/addressDal');
 const chanceDal = require('../dal/chanceDal');
+const streetDal = require('../dal/streetDal');
 const { InsertChanceReq } = require('../models/chance');
+
+const getStreetLocalName = (waMessage, streetsList) => {
+    let streetLocalName;
+
+    streetsList.map(street => {
+        if(waMessage.indexOf(street.LocalName) > -1) {
+            streetLocalName = street.LocalName;
+        }
+    })
+
+    return streetLocalName;
+}
 
 const chanceInsert = async (req, res) => {
     
@@ -9,6 +22,21 @@ const chanceInsert = async (req, res) => {
     let driver = await driverDal.driverGet(req.body.Driver) || {};
     if(!driver.DriverId) {
         driver = await driverDal.driverInsert(req.body.Driver);
+    }
+
+    // check whenever the street is exists in the City:
+    //      if "yes" replace with it the req.body.Address.StreetName
+    //      if "no" log the message and response http status 403
+    const waMessage = req.body.Address.StreetName;
+    const cityStreets = await streetDal.streetGetAll(req.body.Address);
+    const streetLocalName = getStreetLocalName(waMessage, cityStreets)
+    if(streetLocalName) {
+        req.body.Address.StreetName = streetLocalName;
+    } else {
+        // todo: log waMessage
+        console.log(`Error: The street name isn't found in the message "${waMessage}".`);
+        res.statusCode = 403;
+        return {};
     }
     
     // get address
@@ -22,8 +50,8 @@ const chanceInsert = async (req, res) => {
     const chanceReq = new InsertChanceReq (address.AddressId, driver.DriverId, req.body.Chance.DateStart, null, driver.DriverId);
     const chanceRes = await chanceDal.chanceInsert(chanceReq);
 
-    res.statusCode = (200);
+    res.statusCode = 200;
     return chanceRes;
 }
 
-module.exports = {chanceInsert}
+module.exports = { chanceInsert }
