ba5cc8865786743cf476a1a37f25e5960e2b2d12
Simon <simonbor@gmail.com>
2020-08-20 14:23:48 +0300

    driver model update

--- src/models/driver.js
+++ src/models/driver.js
@@ -1,12 +1,13 @@
 'use strict';
 
 class Driver {
-	constructor({DriverId, MobileNum, Name, CarName, CarColor}) {
+	constructor({DriverId, MobileNum, Name, CarName, CarColor, Reports}) {
         this.DriverId = DriverId;
         this.MobileNum = MobileNum;
         this.Name = Name;
         this.CarName = CarName;
         this.CarColor = CarColor;
+        this.Reports = Reports;
     }
 }
 
