d929198060c78bf6f7484f4fe6bec7d2daaff81e
Simon <simonbor@gmail.com>
2020-08-20 11:53:43 +0300

    cors issue

--- src/server.js
+++ src/server.js
@@ -7,8 +7,17 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
+
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET',
+        'Access-Control-Allow-Headers': '*'
+    };
+
     const router = require('./router');
     router.route(req, res);
+
+    //res.writeHead(200, headers);
 });
 
 
