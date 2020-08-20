4fd290b93b097e7875fd57a60c6d8fff7630e1cf
Simon <simonbor@gmail.com>
2020-08-20 14:24:40 +0300

    login controller fix

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,12 +2,17 @@ const jwt = require('jsonwebtoken');
 const driverDal = require('../dal/driverDal');
 
 const driverGet = async (req, res) => {
-    const secret = process.env.CRYPTO_KEY || 'secretPassword';
     const driver = await driverDal.driverGet(req.body.Driver);
+    if (!driver.DriverId) {
+        res.statusCode = 401;
+        return { auth: false, user: {}, token: '' };
+    }
+    
+    const secret = process.env.CRYPTO_KEY || 'secretPassword';
     const token = jwt.sign({ id: driver.DriverId }, secret, { expiresIn: 600 }); // expires in 10 mins
     
     res.statusCode = 200;
-    return { auth: true, user: driver, token: token };
+    return { auth: true, user: JSON.stringify(driver), token: token };
 }
 
 module.exports = { driverGet }
