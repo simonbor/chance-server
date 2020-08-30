2adb70aa0cd470b69ecbd80780c74d94da1516ad
Simon <simonbor@gmail.com>
2020-08-30 19:28:44 +0300

    postgres db context refactor

--- src/dal/dal-context/psqlContext.js
+++ src/dal/dal-context/psqlContext.js
@@ -16,11 +16,15 @@ module.exports = class MssqlContext {
         // console.log(arrParams);
         // console.log(procedureName);
 
-        const data = [];
-        const res = await pool.query(`select row_to_json(${procedureName}(${csDollars}))`, arrParams);
-        res.rows.map(row => { row && data.push(row['row_to_json']) })
+        let data = [], res;
+        try {
+            res = await pool.query(`select row_to_json(${procedureName}(${csDollars}))`, arrParams);
+            res.rows.map(row => { row && data.push(row['row_to_json']) });
+        } catch (err) {
+            console.error(`Error: ${err.message}`);
+        }
+
         pool.end();
-        
         return [data];
     }
 }
