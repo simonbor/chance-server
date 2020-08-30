274e40732061be953ba3bf1b908415f05d284c15
Simon <simonbor@gmail.com>
2020-08-30 15:54:43 +0300

    address flow refactor

--- src/dal/addressDal.js
+++ src/dal/addressDal.js
@@ -2,20 +2,20 @@
 const DbContext = require('./dal-context/dbContext');
 const { Address, GetAddressReq, InsertAddressReq } = require('../models/address');
 
-const addressGet = async function(addressPayload) {
+const addressGet = async function(req) {
     const dbContext = new DbContext();
 
-    const addressReq = new GetAddressReq(addressPayload)
+    const addressReq = new GetAddressReq(req)
     const addressRes = await dbContext.execute('main."sp_GetAddress"', addressReq);
     const address = new Address((addressRes[0])[0] || {});
 
     return address;
 }
 
-const addressInsert = async function(addressPayload) {  
+const addressInsert = async function(req) {
     const dbContext = new DbContext();
 
-    const addressReq = new InsertAddressReq(addressPayload);
+    const addressReq = new InsertAddressReq(req);
     const addressRes = await dbContext.execute('main."sp_InsertAddress"', addressReq);
     const address = new Address((addressRes[0])[0]);
 
--- src/models/address.js
+++ src/models/address.js
@@ -1,9 +1,9 @@
 class Address {
 	constructor({AddressId, StreetLocalName, Building, StreetId, CityId, CountryId, CreatedBy}) {
-		this.AddressId = 	AddressId;
-		this.CountryId = 	CountryId;
-		this.CityId = 		CityId;
-		this.StreetLocalName = 	StreetLocalName;
+		this.AddressId = 		AddressId;
+		this.CountryId = 		CountryId;
+		this.CityId = 			CityId;
+		this.StreetLocalName =	StreetLocalName;
 		this.Building = 		Building;
 		this.StreetId = 		StreetId;
 		this.CreatedBy = 		CreatedBy;
@@ -11,21 +11,21 @@ class Address {
 }
 
 class GetAddressReq{
-	constructor({StreetName, Building,CityId, CountryId}) {
-		this.StreetName = { 'typeName': 'VarChar', 'typeLength': 50, 'value': StreetName };
-		this.CityId = { 'typeName': 'Int', 'value': CityId };
-		this.CountryId = { 'typeName': 'Int', 'value': CountryId };
-		this.Building = { 'typeName': 'Int', 'value': Building };
+	constructor({StreetId, Building,CityId, CountryId}) {
+		this.StreetId = 	{ 'typeName': 'Int', 'value': StreetId };
+		this.CityId = 		{ 'typeName': 'Int', 'value': CityId };
+		this.CountryId = 	{ 'typeName': 'Int', 'value': CountryId };
+		this.Building = 	{ 'typeName': 'Int', 'value': Building };
 	}
 }
 
 class InsertAddressReq {
-	constructor({StreetName, Building,CityId, CountryId, CreatedBy}) {
-		this.StreetName = { 'typeName': 'VarChar', 'typeLength': 50, 'value': StreetName };
-		this.CityId = { 'typeName': 'Int', 'value': CityId };
-		this.CountryId = { 'typeName': 'Int', 'value': CountryId };
-		this.Building = { 'typeName': 'Int', 'value': Building };
-		this.CreatedBy = { 'typeName': 'Int', 'value': CreatedBy };
+	constructor({StreetId, Building, CityId, CountryId, CreatedBy}) {
+		this.StreetId = 	{ 'typeName': 'Int', 'value': StreetId };
+		this.CityId = 		{ 'typeName': 'Int', 'value': CityId };
+		this.CountryId = 	{ 'typeName': 'Int', 'value': CountryId };
+		this.Building = 	{ 'typeName': 'Int', 'value': Building };
+		this.CreatedBy = 	{ 'typeName': 'Int', 'value': CreatedBy };
 	}
 }
 
