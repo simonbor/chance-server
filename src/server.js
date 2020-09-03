224b581aefa7455b756f9a2ffb2b313cb5a40c5e
Simon <simonbor@gmail.com>
2020-09-04 00:27:17 +0300

    cors fix

--- src/server.js
+++ src/server.js
@@ -6,19 +6,24 @@ const HmContext = require('./contexts/hmContext/hmContext');
 
 const host = '0.0.0.0';
 const port = process.env.PORT || 8080;
-const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST, GET',
-    'Access-Control-Allow-Headers': '*',
-    'Content-Type': 'application/json;charset=UTF-8'
-};
 
 const router = require('./router');
 const server = http.createServer();
-server.on('request', async (req, res) => {
-    const jsonString = await router.route(req, res);
 
-    res.writeHead(res.statusCode, headers);
+server.on('request', async (req, res) => {
+    // cors block
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+    // cors trick
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+    
+    const jsonString = await router.route(req, res);
     res.end(JSON.stringify(jsonString));
 });
 
