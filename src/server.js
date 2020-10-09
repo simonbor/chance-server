a97b6154db208d244e9b05a6e499c0c6ea445583
Simon <simonbor@gmail.com>
2020-10-09 10:28:24 +0300

    server fix

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@
 const http = require('http');
 const DbContext = require('./contexts/data/db-context');
 const HmContext = require('./contexts/maps/hmContext');
-const { authenticate } = require('./auth');
+// const { authenticate } = require('./auth');
 const { route } = require('./router');
 const utils = require('./utils');
-const errorHandler = require('./error-handler');
-const { Http401Error } = require('./errors');
-const { ChanceResponse } = require('../src/models/response');
-const{ HttpStatusCode } = require('./enums');
+// const errorHandler = require('./error-handler');
+// const { Http401Error } = require('./errors');
+// const { ChanceResponse } = require('../src/models/response');
+// const{ HttpStatusCode } = require('./enums');
 
 const host = '0.0.0.0';
 const port = process.env.PORT || 8080;
