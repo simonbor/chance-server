3bb4f4f2d58cf79923e98f70b93c85469a9abdfb
Simon Borsky <simonbor@gmail.com>
2020-12-08 10:53:13 +0200

    cache perform

---
+++ src/cache.js
@@ -0,0 +1,30 @@
+const cache = new Map()
+const minute = 1000 * 60; // default living time
+
+// pay attention while use - the item can be expired
+const has = function (key) {
+    return cache.has(key)
+};
+
+const set = function (key, value, ttl = minute) {
+    return cache.set(key, [value, Date.now() + ttl])
+};
+
+const get = function (key) {
+    if(cache.has(key) && cache.get(key)[1] < Date.now()) {
+        return cache.get(key)[0];
+    } else {
+        cache.delete(key);
+        return null;
+    }
+};
+
+const remove = function (key) {
+    return cache.delete(key)
+};
+
+const clear = function () {
+    return cache.clear()
+};
+
+module.exports = { set, get }
